d26169ad548f59ada723cf38402331b5d8f4310a
bernardo.silva <bernardo.silva@agpr5.net>
2025-02-20 08:38:09 -0300

    :sparkles: Add Domain and Infrastructure Projects

---
+++ TechLibrary.Infrastructure/TechLibraryDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TechLibrary.Infrastructure.entities;
+
+namespace TechLibrary.Infrastructure
+{
+    public class TechLibraryDbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=inserir depois");
+        }
+    }
+}
